697bdf9641817d44482f9f2c8eb68520e1379bc8
Alexandre Grégoire <alexandre.gregoire.2@ens.etsmtl.ca>
2018-06-27 21:14:13 -0400

    Added a missing file.

---
+++ p4ignoreinstaller/IgnoreFileInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace p4ignoreinstaller
+{
+    class IgnoreFileInfo
+    {
+
+        public string FilePath { get; set; }
+        public string File
+        {
+            get
+            {
+                if(!String.IsNullOrEmpty(FilePath))
+                {
+                    return FilePath + "\\.p4ignore";
+                }
+                else
+                {
+                    return String.Empty;
+                }
+            }
+        }
+        public string Text { get; set; }
+
+        public IgnoreFileInfo()
+        {
+            FilePath = String.Empty;
+            Text = String.Empty;
+        }
+
+        public IgnoreFileInfo(string Text)
+        {
+            FilePath = String.Empty;
+            this.Text = Text;
+        }
+
+        public IgnoreFileInfo(string FilePath, string Text)
+        {
+            this.FilePath = FilePath;
+            this.Text = Text;
+        }
+    }
+}
